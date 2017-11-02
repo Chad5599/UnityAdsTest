a5c6cea4567333c4ae80a9f12b5ee32e67fdc7f1
“Chad5599” <irtazamumtaz10@gmail.com>
2017-11-02 12:51:45 +0500

    Delete native express ads button as deprecated

--- Assets/Scripts/GameManagerAdmobAds.cs
+++ Assets/Scripts/GameManagerAdmobAds.cs
@@ -2,6 +2,7 @@
 
 using UnityEngine;
 using UnityEngine.UI;
+using GoogleMobileAds.Api;
 
 public class GameManagerAdmobAds : MonoBehaviour
 {
@@ -19,6 +20,11 @@ public class GameManagerAdmobAds : MonoBehaviour
     public Text coins;
 
     private int totalCoins = 0;
+
+    private BannerView bannerView;
+    private InterstitialAd interstitial;
+    private NativeExpressAdView nativeExpressAdView;
+    private RewardBasedVideoAd rewardBasedVideo;
 
     void Start()
     {
@@ -41,11 +47,67 @@ public class GameManagerAdmobAds : MonoBehaviour
         Button btn9 = destroyRewardedVideButton.GetComponent<Button>();
         btn9.onClick.AddListener(DestroyRewardedVide);
 
+
+
+       
+            #if UNITY_ANDROID
+            string appId = "ca-app-pub-3940256099942544~3347511713";
+            #elif UNITY_IPHONE
+            string appId = "ca-app-pub-3940256099942544~1458002511";
+            #else
+            string appId = "unexpected_platform";
+            #endif
+
+            // Initialize the Google Mobile Ads SDK.
+            MobileAds.Initialize(appId);
+
+            // Get singleton reward based video ad reference.
+            this.rewardBasedVideo = RewardBasedVideoAd.Instance;
+
+            // RewardBasedVideoAd is a singleton, so handlers should only be registered once.
+            this.rewardBasedVideo.OnAdLoaded += this.HandleRewardBasedVideoLoaded;
+            this.rewardBasedVideo.OnAdFailedToLoad += this.HandleRewardBasedVideoFailedToLoad;
+            this.rewardBasedVideo.OnAdOpening += this.HandleRewardBasedVideoOpened;
+            this.rewardBasedVideo.OnAdStarted += this.HandleRewardBasedVideoStarted;
+            this.rewardBasedVideo.OnAdRewarded += this.HandleRewardBasedVideoRewarded;
+            this.rewardBasedVideo.OnAdClosed += this.HandleRewardBasedVideoClosed;
+            this.rewardBasedVideo.OnAdLeavingApplication += this.HandleRewardBasedVideoLeftApplication;
+           
     }
 
 
     public void RequestBanner()
     {
+        // These ad units are configured to always serve test ads.
+        #if UNITY_EDITOR
+        string adUnitId = "unused";
+        #elif UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+        #elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+        #else
+        string adUnitId = "unexpected_platform";
+        #endif
+
+        // Clean up banner ad before creating a new one.
+        if (this.bannerView != null)
+        {
+        this.bannerView.Destroy();
+        }
+
+        // Create a 320x50 banner at the top of the screen.
+        this.bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Top);
+
+        // Register for ad events.
+        this.bannerView.OnAdLoaded += this.HandleAdLoaded;
+        this.bannerView.OnAdFailedToLoad += this.HandleAdFailedToLoad;
+        this.bannerView.OnAdOpening += this.HandleAdOpened;
+        this.bannerView.OnAdClosed += this.HandleAdClosed;
+        this.bannerView.OnAdLeavingApplication += this.HandleAdLeftApplication;
+
+        // Load a banner ad.
+        this.bannerView.LoadAd(this.CreateAdRequest());
+
         log.text = "button RequestBanner() pressed";
     }
 
