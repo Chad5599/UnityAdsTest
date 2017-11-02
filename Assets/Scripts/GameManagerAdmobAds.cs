97ad57bcd2ff1305d30570139c9ca4fd0c96c6d9
“Chad5599” <irtazamumtaz10@gmail.com>
2017-11-02 10:27:23 +0500

    Implement Unity Ads

---
+++ Assets/Scripts/GameManagerAdmobAds.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using GoogleMobileAds.Api;
+
+public class GameManagerAdmobAds : MonoBehaviour
+{
+    public Button AdmobAdsButton;
+
+    public Text log;
+    public Text coins;
+
+    private int totalCoins = 0;
+
+    void Start()
+    {
+        Button rewardBtn = AdmobAdsButton.GetComponent<Button>();
+        rewardBtn.onClick.AddListener(ShowRewardedAd);
+
+        SetButtonsFalse();
+
+        StartCoroutine(ShowButtonWhenAdsReady());
+    }
+
+    IEnumerator ShowButtonWhenAdsReady()
+    {
+        while (true)
+            yield return null;
+        AdmobAdsButton.interactable = true;
+        log.text = "Unity Ads Ready let the game earn money. Watch it. Watch it.";
+    }
+
+    public void ShowRewardedAd()
+    {
+       
+    }
+
+    public void SetButtonsFalse()
+    {
+        AdmobAdsButton.interactable = false;
+    }
+}
---
+++ Assets/Scripts/GameManagerUnityAds.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class GameManagerUnityAds : MonoBehaviour
+{
+    public Button UnityRewardedAdsButton;
+    public Button UnitySkipAdsButton;
+
+    public Text log;
+    public Text coins;
+
+    private int totalCoins = 0;
+
+    void Start()
+    {
+        Button rewardBtn = UnityRewardedAdsButton.GetComponent<Button>();
+        rewardBtn.onClick.AddListener(ShowRewardedAd);
+
+        Button skipBtn = UnitySkipAdsButton.GetComponent<Button>();
+        skipBtn.onClick.AddListener(ShowSkipAd);
+
+        SetButtonsFalse();
+
+        StartCoroutine(ShowButtonWhenAdsReady());
+    }
+
+    IEnumerator ShowButtonWhenAdsReady()
+    {
+        while (!Advertisement.IsReady())
+            yield return null;
+        
+        UnityRewardedAdsButton.interactable = false;
+        UnitySkipAdsButton.interactable = false;
+
+        log.text = "Unity Ads Ready let the game earn money. Watch it. Watch it.";
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            log.text = "Unity Rewarded ads button pressed";
+
+            ShowOptions options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    public void ShowSkipAd()
+    {
+        if (Advertisement.IsReady("video"))
+        {
+            log.text = "Unity skip ads button pressed";
+
+            ShowOptions options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("video", options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result) // result is an enum argument
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("The Unity ad was successfully shown.");
+
+                log.text = "The Unity ad was successfully shown.";
+
+                totalCoins += 5;
+                coins.text = totalCoins.ToString();
+
+                SetButtonsFalse();
+
+                StartCoroutine(ShowButtonWhenAdsReady());// again checking when the ads will available to watch.
+
+                break;
+
+            case ShowResult.Skipped:
+                
+                Debug.Log("The Unity ad was skipped before reaching the end.");
+                log.text = "The Unity ad was skipped before reaching the end.";
+
+                SetButtonsFalse();
+
+                break;
+
+            case ShowResult.Failed:
+                
+                Debug.LogError("The Unity ad failed to be shown.");
+                log.text = "The Unity ad failed to be shown.";
+
+                SetButtonsFalse();
+
+                break;
+
+        }
+    }
+
+    public void SetButtonsFalse()
+    {
+        UnityRewardedAdsButton.interactable = false;
+        UnitySkipAdsButton.interactable = false;
+    }
+}
